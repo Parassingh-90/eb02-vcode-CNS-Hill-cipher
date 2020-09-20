326871c8d79819965e9983e380a39acc7335b5ce
kshitizspn <52329885+kshitizspn@users.noreply.github.com>
2020-09-20 22:33:17 +0530

    Update script.js

--- experiment/simulation/js/script.js
+++ experiment/simulation/js/script.js
@@ -19,8 +19,9 @@ document.getElementById("display").style.display="none";
 	document.getElementById("displayopt1").style.display="none";
 	document.getElementById("displayopt2").style.display="none";
 	document.getElementById("Dis1").style.display="none";
+	document.getElementById("encry").style.display="none";
 	
-	var log=2;
+	var log=1;
 	function displaykey()
 	{
 		console.log("harsh");
@@ -58,6 +59,10 @@ document.getElementById("display").style.display="none";
 	{	
 		var str=document.getElementById('f11').value;
 		document.getElementById('pl').innerHTML=str;
+		document.getElementById('k00').innerHTML=document.getElementById('field00').value;
+		document.getElementById('k01').innerHTML=document.getElementById('field01').value;
+		document.getElementById('k10').innerHTML=document.getElementById('field10').value;
+		document.getElementById('k11').innerHTML=document.getElementById('field11').value;
 		if(str=="")
 		{
 			document.getElementById("displayopt2").style.display="block";
@@ -71,9 +76,8 @@ document.getElementById("display").style.display="none";
 		document.getElementById("f1").style.display="none";
 		document.getElementById("f2").style.display="block";
 		document.getElementById("logic2").style.display="none";
-		document.getElementById("logic3").style.display="block";
+		document.getElementById("encry").style.display="block";
 		 document.getElementsByTagName('Submit').style.backgroundColor="gray";
-		document.getElementById("Submit").disabled=true;
 		}
 	}
 	function next()
@@ -84,6 +88,7 @@ document.getElementById("display").style.display="none";
 		var two=parseInt(document.getElementById('field01').value);
 		var three=parseInt(document.getElementById('field10').value);
 		var four=parseInt(document.getElementById('field11').value);
+		
 		var pos=0
 		var res1=0
 		var res2=0
@@ -93,6 +98,8 @@ document.getElementById("display").style.display="none";
 		if(n%2!=0)
 		{
 			str=str.concat("X");
+			n=n+1;
+			
 		}
 		for(var i=0;i<(n/2);i++)
 		{
@@ -104,25 +111,27 @@ document.getElementById("display").style.display="none";
 			cipher=cipher.concat(myalpha[res2]);
 			pos=pos+2;
 		}	
-		document.getElementById("ci").innerHTML=cipher;
-		if(log==2)
+		if(log==(n/2))
 		{	
 			document.getElementById("logic3").style.display="none";
 			document.getElementById("logic4").style.display="block";
+			document.getElementById("encry").style.display="none";
+			document.getElementById("display2").style.display="block";
+			document.getElementById("ci").innerHTML=cipher;
 		}
-		if(log==3)
+		if(log==(n/2)+1)
 		{
 			document.getElementById("logic4").style.display="none";
 			document.getElementById("logic5").style.display="block";
-			document.getElementById("display2").style.display="block";
 		}
-		if(log==4)
+		if(log==(n/2)+2)
 		{
 			document.getElementById("f2").style.display="none";
 			document.getElementById("f1").style.display="none";
 			document.getElementById("f3").style.display="block";
 			document.getElementById("logic5").style.display="none";
 			document.getElementById("logic6").style.display="block";
+			
 		}
 			
 		log=log+1;
@@ -150,7 +159,7 @@ document.getElementById("display").style.display="none";
 	}
 	function reset()
 	{	
-		log=2;
+		log=1;
 		document.getElementById('field00').value="";
 		document.getElementById('field01').value="";
 		document.getElementById('field10').value="";
@@ -182,6 +191,7 @@ document.getElementById("display").style.display="none";
 		document.getElementById("pst").style.display="none";
 		document.getElementById("displayopt1").style.display="none";
 		document.getElementById("displayopt2").style.display="none";
+		document.getElementById("encry").style.display="none";
 	
 	}
 	function viewresult2()
@@ -344,8 +354,3 @@ document.getElementById("display").style.display="none";
 
 	
 
-
-	
-
-
-	
